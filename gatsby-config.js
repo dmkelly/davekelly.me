6084d598d7dff2acb846852327deb5637b6c0a41
Dave Kelly <dmk3534@gmail.com>
2020-01-25 08:00:11 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        anonymize: true,
+        respectDNT: true,
       },
     },
     {
